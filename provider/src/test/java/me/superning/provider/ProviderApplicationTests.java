869e9e8784e65de38b17450de9ab797131ccf5c8
superning <xining.l.se@gmail.com>
2020-01-15 10:58:31 +0800

    第一次提交

---
+++ provider/src/test/java/me/superning/provider/ProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package me.superning.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
